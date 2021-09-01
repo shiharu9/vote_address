070a3a058bfc9feda0665fabf4ea3ad94f8121e2
shiharu9 <shiharu.nika@gmail.com>
2021-09-02 00:15:04 +0300

    Create models "Address" and "Choice"
    *Add models in admin.py

--- vote/admin.py
+++ vote/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['country', 'city', 'street', 'house', 'residents', 'question_text']
+    list_display_links = ['country', 'city', 'street']
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Address, AddressAdmin)
--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    country = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    street = models.CharField(max_length=40)
+    house = models.CharField(max_length=10)
+    residents = models.IntegerField(default=0)
+    question_text = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Address, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
